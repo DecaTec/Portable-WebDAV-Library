24d6624f6a7553ffd3293b4205bc05db104f4972
DecaTec <jr@decatec.de>
2016-04-08 17:12:12 +0200

    Updated NetFx documentation, set assembly version to v0.3.0.0

--- NetFx/DecaTec.WebDav.NetFx/Properties/AssemblyInfo.cs
+++ NetFx/DecaTec.WebDav.NetFx/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DecaTec.WebDav")]
+[assembly: AssemblyTitle("DecaTec.WebDav.NetFx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DecaTec")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
