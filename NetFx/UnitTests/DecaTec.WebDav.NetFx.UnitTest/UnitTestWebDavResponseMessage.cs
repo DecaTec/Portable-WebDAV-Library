73c5c21cb5b51e2e5089facf516e3f77bf7868ff
DecaTec <jr@decatec.de>
2017-02-16 17:32:36 +0100

    Unit test for transparently wrap headers

--- NetFx/UnitTests/DecaTec.WebDav.NetFx.UnitTest/UnitTestWebDavResponseMessage.cs
+++ NetFx/UnitTests/DecaTec.WebDav.NetFx.UnitTest/UnitTestWebDavResponseMessage.cs
@@ -1,20 +1,21 @@
 ﻿using System.Net.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace DecaTec.WebDav.NetFx.UnitTest
 {
 	[TestClass]
 	public class UnitTestWebDavResponseMessage
 	{
-		[TestMethod]
-		public void UT_NetFx_WebDavResponseMessage_Transparently_Wrap_Headers()
-		{
-			const string expected = "Jetty(9.3.9.v20160517)";
-			var responseToWrap = new HttpResponseMessage();
-			responseToWrap.Headers.TryAddWithoutValidation("Server", expected);
+        [TestMethod]
+        public void UT_NetFx_WebDavResponseMessage_Transparently_Wrap_Headers()
+        {
+            const string expected = "Jetty(9.3.9.v20160517)";
+            var responseToWrap = new HttpResponseMessage();
+            responseToWrap.Headers.TryAddWithoutValidation("Server", expected);
 
-			var wrapper = new WebDavResponseMessage(responseToWrap);
-			var actual = wrapper.Headers.GetValues("Server").ToString();
+            var wrapper = new WebDavResponseMessage(responseToWrap);
+            var actual = wrapper.Headers.GetValues("Server").FirstOrDefault();
 
 			Assert.AreEqual(expected, actual);
 		}
