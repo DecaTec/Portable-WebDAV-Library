9e00fd373eed48d51738e5b8e56911295c2e9a7c
DecaTec <jr@decatec.de>
2017-02-27 19:56:33 +0100

    Added unit test for LockInfo

---
+++ NetFx/UnitTests/DecaTec.WebDav.NetFx.UnitTest/UnitTestLockInfo.cs
@@ -0,0 +1,21 @@
+﻿using DecaTec.WebDav.WebDavArtifacts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Xml.Linq;
+
+namespace DecaTec.WebDav.NetFx.UnitTest
+{
+    [TestClass]
+    public class UnitTestLockInfo
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void UT_NetFx_LockInfo_ThrowsInvalidOperationExceptionWhenOwnerRawIsSetAndOwnerHrefShouldBeSet()
+        {
+            var lockInfo = new LockInfo();
+            var ownerRawString = "<owner xmlns=\"DAV:\"><href>http://example.org/~ejw/contact.html</href><x:author xmlns:x=\"http://example.com/ns\"><x:name>Jane Doe</x:name></x:author></owner>";
+            lockInfo.OwnerRaw = XElement.Parse(ownerRawString);
+            lockInfo.OwnerHref = "test@test.com";
+        }
+    }
+}
